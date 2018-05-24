8f771c6178c030a87cbcf7e72b9d662242092ff5
y4izus <yaiza.garcia.mm@gmail.com>
2018-05-24 16:24:48 +0200

    Show GeoJSON LineString data = PK_autopistas

--- react-leaflet/src/LeafletMap.js
+++ react-leaflet/src/LeafletMap.js
@@ -1,20 +1,17 @@
 import React, { Component } from "react"
 import { Map, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet"
 import { ejes } from '../../data/ejes.geojson'
+import { pk_autopistas } from '../../data/pk_autopistas_ej_clean.geojson'
 
 export class LeafletMap extends Component {
   constructor() {
-    console.log(ejes.name)
     super()
+    // console.log(this.pk_autopistas)
     this.state = {
       lat: 40.4165,
       lng: -3.70256,
       zoom: 6
     }
-  }
-
-  getGeoJson(){
-    return ejes;
   }
 
   render() {
@@ -25,14 +22,8 @@ export class LeafletMap extends Component {
           attribution='&copy <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
-          <Popup>
-            <span>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </span>
-          </Popup>
-        </Marker>
-        <GeoJSON data={this.getGeoJson()}></GeoJSON>
+        <GeoJSON data={ ejes }></GeoJSON>
+        <GeoJSON data={ pk_autopistas }></GeoJSON>
       </Map>
     )
   }
